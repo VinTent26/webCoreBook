fc3137822fbb1052fee6524620c330d8da4e972d
PhucThien04 <phucthiensh@gmail.com>
2024-12-05 16:43:45 +0700

    init

---
+++ webCore/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webCore.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Payment1()
+        {
+            return View();
+        }
+    }
+}
